43af1564a15dda5f42e7e505dd4ed6a7547f15cf
ZoOL <zhooul@gmail.com>
2025-05-28 18:17:05 +0800

    ✨ feat(main): enhance Bevy app setup with custom window and asset configurations
    - set `ClearColor` to light gray for consistent background
    - configure primary window with title, canvas binding, and parent fitting
    - disable default event handling to allow browser shortcuts
    - set `AssetMetaCheck::Never` to optimize asset loading in development

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,28 @@
-use bevy::prelude::*;
+use bevy::{asset::AssetMetaCheck, prelude::*};
 use evershard::EverShardPlugin;
 
 fn main() -> AppExit {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .insert_resource(ClearColor(Color::linear_rgb(0.4, 0.4, 0.4)))
+        .add_plugins(
+            DefaultPlugins
+                .set(WindowPlugin {
+                    primary_window: Some(Window {
+                        title: "EverShard".to_string(),
+                        // Bind to canvas included in `index.html`
+                        canvas: Some("#bevy".to_owned()),
+                        fit_canvas_to_parent: true,
+                        // Tells wasm not to override default event handling, like F5 and Ctrl+R
+                        prevent_default_event_handling: false,
+                        ..default()
+                    }),
+                    ..default()
+                })
+                .set(AssetPlugin {
+                    meta_check: AssetMetaCheck::Never,
+                    ..default()
+                }),
+        )
         .add_plugins(EverShardPlugin)
         .run()
 }
